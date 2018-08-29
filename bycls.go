be9443db1f10d0f1adc45dbff2d7cb7d181c9aa0
Titus Tienaah <titus.intdxdt@gmail.com>
2018-08-29 14:57:40 -0300

    cache map

--- bycls.go
+++ bycls.go
@@ -1,24 +1,26 @@
 package constrain
 
 import (
-    "github.com/TopoSimplify/opts"
-    "github.com/TopoSimplify/node"
-    "github.com/TopoSimplify/deform"
-    "github.com/TopoSimplify/hdb"
+	"github.com/TopoSimplify/opts"
+	"github.com/TopoSimplify/node"
+	"github.com/TopoSimplify/deform"
+	"github.com/TopoSimplify/hdb"
+	"github.com/TopoSimplify/cmap"
 )
 
 //Constrain for self-intersection as a result of simplification
 //returns boolean : is hull collapsible
-func ByFeatureClassIntersection(options *opts.Opts, hull *node.Node, hulldb *hdb.Hdb, selections *[]*node.Node) bool {
-    var bln = true
-    //find hull neighbours
-    var hulls = deform.SelectFeatureClass(options, hulldb, hull)
-    for _, h := range hulls {
-        //if bln & selection contains current hull : bln : false
-        if bln && (h == hull) {
-            bln = false // cmp ref
-        }
-        *selections = append(*selections, h)
-    }
-    return bln
+func ByFeatureClassIntersection(options *opts.Opts, hull *node.Node, hulldb *hdb.Hdb,
+	selections *[]*node.Node, cache *cmap.CacheMap) bool {
+	var bln = true
+	//find hull neighbours
+	var hulls = deform.SelectFeatureClass(options, hulldb, hull, cache)
+	for _, h := range hulls {
+		//if bln & selection contains current hull : bln : false
+		if bln && (h == hull) {
+			bln = false // cmp ref
+		}
+		*selections = append(*selections, h)
+	}
+	return bln
 }
--- bycls_test.go
+++ bycls_test.go
@@ -3,14 +3,15 @@ package constrain
 import (
 	"time"
 	"testing"
+	"github.com/intdxdt/iter"
+	"github.com/franela/goblin"
 	"github.com/TopoSimplify/dp"
+	"github.com/TopoSimplify/hdb"
 	"github.com/TopoSimplify/node"
 	"github.com/TopoSimplify/opts"
 	"github.com/TopoSimplify/offset"
-	"github.com/franela/goblin"
-	"github.com/TopoSimplify/hdb"
-	"github.com/intdxdt/iter"
 	"github.com/TopoSimplify/common"
+	"github.com/TopoSimplify/cmap"
 )
 
 func TestByFeatureClassIntersection(t *testing.T) {
@@ -22,8 +23,8 @@ func TestByFeatureClassIntersection(t *testing.T) {
 
 			var options = &opts.Opts{MinDist: 10}
 			var coords = common.LinearCoords("LINESTRING ( 780 600, 740 620, 720 660, 720 700, 760 740, 820 760, 860 740, 880 720, 900 700, 880 660, 840 680, 820 700, 800 720, 760 700, 780 660, 820 640, 840 620, 860 580, 880 620, 820 660 )")
-			var hulls = common.CreateHulls(id,[][]int{{0, 3}, {3, 8}, {8, 13}, {13, 17}, {17, coords.Len() - 1}}, coords)
-			var inst = dp.New(coords, options, offset.MaxOffset)
+			var hulls = common.CreateHulls(id, [][]int{{0, 3}, {3, 8}, {8, 13}, {13, 17}, {17, coords.Len() - 1}}, coords)
+			var inst = dp.New(id.Next(), coords, options, offset.MaxOffset)
 
 			for i := range hulls {
 				hulls[i].Instance = inst
@@ -32,7 +33,7 @@ func TestByFeatureClassIntersection(t *testing.T) {
 			var sels = []*node.Node{}
 
 			coords = common.LinearCoords("LINESTRING ( 760 660, 800 620, 800 600, 780 580, 720 580, 700 600 )")
-			hulls = common.CreateHulls(id,[][]int{{0, coords.Len() - 1}}, coords)
+			hulls = common.CreateHulls(id, [][]int{{0, coords.Len() - 1}}, coords)
 			for i := range hulls {
 				hulls[i].Instance = inst
 			}
@@ -40,7 +41,7 @@ func TestByFeatureClassIntersection(t *testing.T) {
 
 			var q1 = hulls[0]
 			coords = common.LinearCoords("LINESTRING ( 680 640, 660 660, 640 700, 660 740, 720 760, 740 780 )")
-			hulls = common.CreateHulls(id,[][]int{{0, coords.Len() - 1}}, coords)
+			hulls = common.CreateHulls(id, [][]int{{0, coords.Len() - 1}}, coords)
 
 			for i := range hulls {
 				hulls[i].Instance = inst
@@ -48,9 +49,10 @@ func TestByFeatureClassIntersection(t *testing.T) {
 			db.Load(hulls)
 
 			var q2 = hulls[0]
+			var cache = cmap.NewCacheMap(2)
 
-			g.Assert(ByFeatureClassIntersection(options, &q1, db, &sels)).IsFalse()
-			g.Assert(ByFeatureClassIntersection(options, &q2, db, &sels)).IsTrue()
+			g.Assert(ByFeatureClassIntersection(options, &q1, db, &sels, cache)).IsFalse()
+			g.Assert(ByFeatureClassIntersection(options, &q2, db, &sels, cache)).IsTrue()
 
 		})
 	})
